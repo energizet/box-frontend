1e213d749348a59c805363e88c2b144939d5530d
energizet <a2437@yandex.ru>
2023-11-12 06:17:19 +0300

    download file

--- src/components/File.js
+++ src/components/File.js
@@ -16,34 +16,66 @@ const File = () => {
         <Suspense fallback={<div>Loading...</div>}>
             <Await resolve={file} errorElement={<ErrorFile/>}>
                 {
-                    file => file == null ?
-                        <div>Not found</div> :
-                        <div id="file">
-                            <div>File: {file.title}</div>
-                            <div>For user:</div>
-                            <img src={file.vkUser.photo} alt="user photo"/>
-                            <div>{file.vkUser.firstName} {file.vkUser.lastName}</div>
-                        </div>
+                    file => FilePage(file)
                 }
             </Await>
         </Suspense>
-
     );
 };
 
+function FilePage(file) {
+    if (file == null) {
+        return <div>Not found</div>
+    }
+
+    if (file instanceof Blob) {
+        let url = URL.createObjectURL(file);
+
+        if (file.type.startsWith('image') === false) {
+            let a = document.createElement('a');
+            a.href = url;
+            a.download = file.type.replace('/', '.');
+            a.click();
+
+            return <div>Loading...</div>;
+        }
+
+        return <img src={url} alt="user photo"/>
+    }
+
+
+    return <div id="file">
+        <div>File: {file.title}</div>
+        <div>For user:</div>
+        <img src={file.vkUser.photo} alt="avatar"/>
+        <div>{file.vkUser.firstName} {file.vkUser.lastName}</div>
+    </div>
+}
+
 File.loader = async (params) => {
-    let file = await fetch(process.env.REACT_APP_API_URL + `/file/${params.id}`,
+    let file = await fetch(process.env.REACT_APP_API_URL + `/file/${params.id}/download`,
         {
             headers: {
                 'Content-Type': 'application/json',
                 'Authorization': 'Bearer ' + sessionStorage.getItem('jwt'),
             },
         });
-    if (file.ok === false) {
+
+    if (file.ok) {
+        return await file.blob();
+    }
+
+    let info = await fetch(process.env.REACT_APP_API_URL + `/file/${params.id}`,
+        {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+    if (info.ok === false) {
         return null;
     }
 
-    return await file.json();
+    return await info.json();
 };
 
 export default File;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -9,7 +9,6 @@ const Nav = () => {
     return (
         <nav>
             <NavLink to="/upload" style={navStyle}>Upload</NavLink>
-            <NavLink to="/file/135ba2f2-03af-44b6-be44-e923d585b6d2" style={navStyle}>new3.png</NavLink>
         </nav>
     );
 };

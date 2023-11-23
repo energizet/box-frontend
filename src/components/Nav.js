526aff5870f899b13261dbe535db9aafe98dafd5
energizet <a2437@yandex.ru>
2023-11-23 18:35:41 +0300

    test

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -9,6 +9,7 @@ const Nav = () => {
     return (
         <nav>
             <NavLink to="/" style={navStyle}>Upload</NavLink>
+            <NavLink to="/" style={navStyle}>Upload</NavLink>
         </nav>
     );
 };

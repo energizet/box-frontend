d043148b1b55f468258d53f7a420002eb8c974d7
ArturRdix <131528966+ArturRdix@users.noreply.github.com>
2023-11-24 13:49:32 +0300

    test

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import {NavLink} from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Nav = () => {
-    let navStyle = ({isActive}) => ({
+    let navStyle = ({ isActive }) => ({
         color: isActive ? 'red' : 'black',
     });
-    
+
     return (
         <nav>
+            <NavLink to="/" style={navStyle}>Upload</NavLink>
             <NavLink to="/" style={navStyle}>Upload</NavLink>
         </nav>
     );
